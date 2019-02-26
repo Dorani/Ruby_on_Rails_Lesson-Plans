f1fd8fbc9123aaef29a12bfdee6c14bc85a771e9
Seif <seif.dorani@gmail.com>
2019-02-26 10:41:38 -0800

    basic ruby built in functions, string data types

---
+++ 01-Introduction/intro1.rb
@@ -0,0 +1,9 @@
+puts "hello world"
+#put string = print out the argument
+#built in function, it will return something
+
+p "hello world"
+# the value will not return nill, returns the argument alone
+
+print "hello world"
+# does not have a new line after it prints whatever argument you give it
