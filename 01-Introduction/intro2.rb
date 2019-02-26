b8cbc207a2f03b0bc6feb9987c54126010440267
Seif <seif.dorani@gmail.com>
2019-02-26 10:52:56 -0800

    more on strings

---
+++ 01-Introduction/intro2.rb
@@ -0,0 +1,18 @@
+#String concatenation
+#be mindful with single quotes you can use string interpolation
+first_name = "seif"
+last_name = "delta"
+puts first_name + " " + last_name
+
+
+#String interpolation
+puts "My first name is #{first_name} and my is #{last_name}"
+
+
+#Methods, how to find them
+
+#Common Methods
+
+#Variable Assigment
+
+#Escaping
