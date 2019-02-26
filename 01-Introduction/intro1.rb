906cf0c0e1be6c3a5e346a818a94ff1880e4091b
Seif <seif.dorani@gmail.com>
2019-02-26 10:45:52 -0800

    variables and methods

--- 01-Introduction/intro1.rb
+++ 01-Introduction/intro1.rb
@@ -7,3 +7,23 @@ p "hello world"
 
 print "hello world"
 # does not have a new line after it prints whatever argument you give it
+
+##-----------------------------------------------------
+
+
+#Variable is a name that we give to a reference to some location in memory
+
+greeting = "hello world"
+puts greeting
+
+##-----------------------------------------------------
+
+#Define a method to say hello, the method takes an argument
+#def and method name, dont forget the end, within it is the code
+
+def say_hello
+  puts "whats up"
+end
+
+# dont forget to call the method:
+say_hello
