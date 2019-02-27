6a590f193b908ac323518251549cc5a7b41f8eef
Seif <seif.dorani@gmail.com>
2019-02-27 00:03:18 -0800

    more on methods

--- 01-Introduction/06-Methods.rb
+++ 01-Introduction/06-Methods.rb
@@ -3,6 +3,18 @@
 def multiply(num1,num2)
   num1.to_f * num2.to_f
 end
+
+def devide(num1,num2)
+  num1.to_f / num2.to_f
+end
+
+def substract(num1,num2)
+  num1.to_f - num2.to_f
+end
+
+def add(num1,num2)
+  num1.to_f + num2.to_f
+end
 #this method when called will return
 #num1 times num2, the return is implied
 #arguments need to be passed in when invoking
@@ -17,3 +29,12 @@ number2 = gets.chomp
 
 puts "the first num multiplied by the second
 is #{multiply(number1, number2)}"
+
+puts "the first num plus by the second
+is #{add(number1, number2)}"
+
+puts "the first num devided by the second
+is #{devide(number1, number2)}"
+
+puts "the first num minus by the second
+is #{substract(number1, number2)}"
