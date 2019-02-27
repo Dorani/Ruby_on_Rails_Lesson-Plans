9e11d087dcf573b16135e906564c93887b26b2f4
Seif <seif.dorani@gmail.com>
2019-02-26 23:45:49 -0800

    edits

--- 01-Introduction/04-Integers.rb
+++ 01-Introduction/04-Integers.rb
@@ -1,37 +1,37 @@
-# #simple math
-# puts 10 / 2
-#
-# puts 2 * 5
-#
-# puts 5 - 4
-#
-# puts 10 / 4
-# #this will return 2, cause it removes the decimal
-# #we can fix this:
-#
-# puts 10 / 4.to_f
-# #.to_f is the method for floats, which converts it to a float
-#
-#
-#
-# #store numbers in vars and do some math!
-# x = 5
-# y = 10
-# puts y / x
-#
-#
-# #string concatenation allows this awesome feature
-# puts "-" * 20
-#
-# #another cool way
-# 20.times {print "-"}
-# 20.times {puts "hello loves"}
-#
-# #how about displaying, 20 random numbers between 0-9
-# 20.times {puts rand(10)}
-#
-# #rand will generate a rand number between 0-1
-#
+#simple math
+puts 10 / 2
+
+puts 2 * 5
+
+puts 5 - 4
+
+puts 10 / 4
+#this will return 2, cause it removes the decimal
+#we can fix this:
+
+puts 10 / 4.to_f
+#.to_f is the method for floats, which converts it to a float
+
+
+
+#store numbers in vars and do some math!
+x = 5
+y = 10
+puts y / x
+
+
+#string concatenation allows this awesome feature
+puts "-" * 20
+
+#another cool way
+20.times {print "-"}
+20.times {puts "hello loves"}
+
+#how about displaying, 20 random numbers between 0-9
+20.times {puts rand(10)}
+
+#rand will generate a rand number between 0-1
+
 
 
 
