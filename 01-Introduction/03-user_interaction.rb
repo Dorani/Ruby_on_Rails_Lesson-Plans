0c5b21de4478f2740fc61e2d44f5b3554233ecf7
Seif <seif.dorani@gmail.com>
2019-02-26 23:29:48 -0800

    integers

--- 01-Introduction/03-user_interaction.rb
+++ 01-Introduction/03-user_interaction.rb
@@ -15,3 +15,14 @@ puts number.to_i * 2
 
 #this is important otherwise it will return the result of adding 2 strings
 #dont forget to conver to the appropriate data type
+
+
+#Quick activity:
+puts "Enter your first name"
+first_name = gets.chomp
+puts "Enter your last name"
+last_name = gets.chomp
+full_name = first_name + " " + last_name
+puts "Your full name is #{full_name}"
+puts "Your full name reversed is #{full_name.reverse}"
+puts "Your name has #{full_name.length - 1} characters in it"
---
+++ 01-Introduction/04-Integers.rb
@@ -0,0 +1,20 @@
+#simple math
+puts 10 / 2
+
+puts 2 * 5
+
+puts 5 - 4
+
+puts 10 / 4
+#this will return 2, cause it removes the decimal
+#we can fix this:
+
+puts 10 / 4.to_f
+#.to_f is the method for floats, which converts it to a float
+
+
+
+#store numbers in vars and do some math!
+x = 5
+y = 10
+puts y / x
