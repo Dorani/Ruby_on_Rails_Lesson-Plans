18b1f7355d5cd2f0f959097718839331e4571698
Seif <seif.dorani@gmail.com>
2019-02-27 00:07:48 -0800

    conditionals

---
+++ 01-Introduction/07-Conditionals.rb
@@ -0,0 +1,14 @@
+#Branching
+#when this condition of true is met, it skips all and goes to end
+if true
+  puts "hello"
+else
+  puts "bye"
+end
+
+#because this is false, it will go to the else and print bye
+if !true
+  puts "hello"
+else
+  puts "bye"
+end
