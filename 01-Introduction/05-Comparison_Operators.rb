5afd143d3c6f6e640f7b3d20af75dc23905af023
Seif <seif.dorani@gmail.com>
2019-02-26 23:50:50 -0800

    comparison ops

---
+++ 01-Introduction/05-Comparison_Operators.rb
@@ -0,0 +1,12 @@
+==
+!=
+>
+<
+
+#standard for the most part
+#where it gets interesting:
+10 = 10 #true
+10 = 10.0 #true
+#to make sure data types match use this:
+10.eql?(10.0)#False
+#this will compare the types
