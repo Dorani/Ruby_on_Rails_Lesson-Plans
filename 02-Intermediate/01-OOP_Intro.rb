286ad259a6a7507ffcd1f0eb5d3eb1a37fbe6f93
Seif <seif.dorani@gmail.com>
2019-02-28 14:46:44 -0800

    new topics, oop

---
+++ 02-Intermediate/01-OOP_Intro.rb
@@ -0,0 +1,10 @@
+#We work with "things" ie objects created from a class
+#Each instance will inherit so called features, ie:
+  #properties(js) or attributes(ruby) which are all specified in the class
+
+
+#Student is a class
+  # attributes: fist name, last name, username, password
+
+#Course is a class
+  # attributes: course_id, short name, name, description
