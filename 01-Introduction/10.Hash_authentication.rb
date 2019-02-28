21d137c796b969cd656ce0f9cba40986e251acd4
Seif <seif.dorani@gmail.com>
2019-02-28 14:13:42 -0800

    refactored authenticator with method and cleaned somethings up

--- 01-Introduction/10.Hash_authentication.rb
+++ 01-Introduction/10.Hash_authentication.rb
@@ -6,9 +6,20 @@ users = [
   {username: "sam", password:"password3"},
   {username: "stacy", password:"password4"}
 ]
+#authentication
+#compare each row to what is entered with a loop on users
+#and some comparisons for validations
+def auth_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record
+    end
+  end
+   "Credentials are not correct"
+end
+
 #this is how you get a value of an arrays objects key
 puts "Welcome" + " " + users[0][:username]
-
 puts "This is our authenticator"
 25.times { print "-"}
 puts
@@ -19,27 +30,16 @@ puts "If password is correct, you will get back the user object"
 
 #during each loop attempts increase by 1
 #until they get to 4 where it stops
-
 attempts = 1
 while attempts < 4
   print "Username: "
   username = gets.chomp
+
   print "Password: "
   password = gets.chomp
 
-
-  #authentication
-  #compare each row to what is entered with a loop on users
-  #and some comparisons for validations
-  users.each do |user|
-    if user[:username] == username && user[:password] == password
-      puts user
-      break
-    else
-      puts "credentials were not correct"
-    end
-  end
-
+  authentication = auth_user(username,password, users)
+  puts authentication
 
   puts "Press n to quit or any key to continue"
   input = gets.chomp.downcase
