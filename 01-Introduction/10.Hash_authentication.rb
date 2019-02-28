5ba3ff4f6e76093a1a7f93d813f61c522ae7c795
Seif <seif.dorani@gmail.com>
2019-02-28 13:53:19 -0800

    while loop for attempts

--- 01-Introduction/10.Hash_authentication.rb
+++ 01-Introduction/10.Hash_authentication.rb
@@ -7,10 +7,21 @@ users = [
   {username: "stacy", password:"password4"}
 ]
 #this is how you get a value of an arrays objects key
-puts users[0][:username]
+puts "Welcome" + " " + users[0][:username]
 
-puts "Welcome to the authenticator"
+puts "This is our authenticator"
 25.times { print "-"}
 puts
 puts "This program will take input from the user and compare passwords"
 puts "If password is correct, you will get back the user object"
+
+
+
+#during each loop attempts increase by 1
+#until they get to 4 where it stops
+
+attempts = 1
+while attempts < 4
+  puts "this will print"
+  attempts += 1
+end
