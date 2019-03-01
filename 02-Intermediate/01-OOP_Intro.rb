beaf74a644fca910bbe82a01ccdd79c4c2a44f33
Seif <seif.dorani@gmail.com>
2019-03-01 11:32:58 -0800

    create student class with geters and setters and created instance with output

--- 02-Intermediate/01-OOP_Intro.rb
+++ 02-Intermediate/01-OOP_Intro.rb
@@ -8,3 +8,40 @@
 
 #Course is a class
   # attributes: course_id, short name, name, description
+
+#Classes have a method called to_s, so by default it will always print
+#something
+
+
+class Student
+  #gives both getters and setters for what we specify
+  attr_accessor :first_name, :last_name, :user_name, :email, :password
+
+  #for just getter functionality use, READ ONLY:
+  #attr_reader :username
+
+  #initialize method will set up our object with attr values
+  def initialize(first_name, last_name, user_name, email, password)
+    @first_name = first_name
+    @last_name = last_name
+    @user_name = user_name
+    @email = email
+    @password = password
+  end
+
+  #method to display object attr
+  def to_s
+    "First name: #{first_name},
+     Last name: #{last_name}
+     User name: #{user_name}
+     Email: #{email}
+     password: #{password}"
+  end
+
+end
+
+seif = Student.new("seif", "dorani", "sdot", "sd@ie.com", "password1")
+john = Student.new("john", "doe", "sdot", "sd@ie.com", "password2")
+
+puts seif
+puts john
