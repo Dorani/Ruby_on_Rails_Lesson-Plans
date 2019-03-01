3c3deeb9837c39278e9476dc44161283daa46a97
Seif <seif.dorani@gmail.com>
2019-03-01 12:04:31 -0800

    bcrypt example

--- 02-Intermediate/03-Classes_Modules_Mixins(part1).rb
+++ 02-Intermediate/03-Classes_Modules_Mixins(part1).rb
@@ -6,3 +6,21 @@
 # Hashed digests are stored in dbs
 # In popular hashing algorithm in MD5 - bcrypt
 # Creates a hash digest of the string as password
+
+
+
+
+#obtained from the docs, require tech
+require 'bcrypt'
+
+my_password = BCrypt::Password.create("my password")
+#=> "$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey"
+puts my_password
+puts my_password.version              #=> "2a"
+puts my_password.cost                 #=> 12
+puts my_password == "my password"     #=> true
+puts my_password == "not my password" #=> false
+
+# my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
+# my_password == "my password"     #=> true
+# my_password == "not my password" #=> false
