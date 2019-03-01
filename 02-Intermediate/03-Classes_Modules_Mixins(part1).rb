7cbc6f0f06bc35f98568e7e8a6d3cf16e1524742
Seif <seif.dorani@gmail.com>
2019-03-01 11:44:48 -0800

    notes on encryption

---
+++ 02-Intermediate/03-Classes_Modules_Mixins(part1).rb
@@ -0,0 +1,8 @@
+# User case for student class - authentication systems in web applications
+# For now think of students as users
+# Done with usernames and passwords
+# Security for user information is very important
+# Passwords are virtually never stores in their string forms
+# Hashed digests are stored in dbs
+# In popular hashing algorithm in MD5 - bcrypt
+# Creates a hash digest of the string as password
