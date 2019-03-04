6fe5a51b439264dffc38718e8af5f198a59dff1c
Seif <seif.dorani@gmail.com>
2019-03-03 22:18:03 -0800

    edits to users data, now using it in bcrypt example to create users and validate with oop

--- 02-Intermediate/04-Crud.rb
+++ 02-Intermediate/04-Crud.rb
@@ -1,8 +1,22 @@
 require 'bcrypt'
 
+
+#users data:
+users = [
+  {username: "seif", password:"password1"},
+  {username: "dan", password:"password2"},
+  {username: "sam", password:"password3"},
+  {username: "stacy", password:"password4"},
+  {username: "carl", password:"password5"}
+]
+
+
+#creation method
 def create_hash_digest(password)
   BCrypt::Password.create(password)
 end
 
-new_password = create_hash_digest("password1")
-puts new_password
+#verify method
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
