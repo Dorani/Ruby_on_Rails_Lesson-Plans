e7180974af0d61db9a76755ff57faf3a830fbff4
Seif <seif.dorani@gmail.com>
2019-03-03 23:04:43 -0800

    created crub module, ie toolkit of built in methods i will get to user elsewhere

---
+++ 02-Intermediate/05-Users_Crud_Toolkit-Module(Part2).rb
@@ -0,0 +1,34 @@
+require 'bcrypt'
+
+
+module Crud
+  #creation method
+  def create_hash_digest(password)
+    BCrypt::Password.create(password)
+  end
+
+  #verify method
+  def verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+
+  #hash passwords with salt tech
+  def create_secure_users(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
+    end
+    list_of_users
+  end
+
+
+  #authenticate users
+  def authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:username] == username &&
+        verify_hash_digest(user_record[:password]) == password
+        return user_record
+      end
+    end
+    "Credentials are not correct"
+  end
+end
