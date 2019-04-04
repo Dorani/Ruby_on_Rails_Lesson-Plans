9fd67fca022a94a776b6130e794c4ad7ddc9e7b8
Seif <seif.dorani@gmail.com>
2019-04-03 23:23:54 -0700

    delete functionality with buttons, validation, message

--- 02-Rails/02-Second_Rails_App/AlphaBlog/app/controllers/articles_controller.rb
+++ 02-Rails/02-Second_Rails_App/AlphaBlog/app/controllers/articles_controller.rb
@@ -40,6 +40,11 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 
   private
     def article_params
