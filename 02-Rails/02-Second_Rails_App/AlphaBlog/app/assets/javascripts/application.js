9db822f1688a2046f10ca603788d664b060ef02c
Seif <seif.dorani@gmail.com>
2019-04-21 23:10:37 -0700

    more styling

--- 02-Rails/02-Second_Rails_App/AlphaBlog/app/assets/javascripts/application.js
+++ 02-Rails/02-Second_Rails_App/AlphaBlog/app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -11,8 +12,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require jquery
-//= require bootstrap
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
