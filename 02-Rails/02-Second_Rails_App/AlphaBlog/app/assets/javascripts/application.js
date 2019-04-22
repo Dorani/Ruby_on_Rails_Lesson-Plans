8b563630bba2cadc0ca2f49f0cdde3248b8f4c62
Seif <seif.dorani@gmail.com>
2019-04-21 19:41:18 -0700

    fixed front end bugs

--- 02-Rails/02-Second_Rails_App/AlphaBlog/app/assets/javascripts/application.js
+++ 02-Rails/02-Second_Rails_App/AlphaBlog/app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require rails-ujs
 //= require activestorage
-require jquery
-require bootstrap-sprockets
+require("jquery3")
+require("popper")
+require("bootstrap-sprockets")
 //= require turbolinks
 //= require_tree .
