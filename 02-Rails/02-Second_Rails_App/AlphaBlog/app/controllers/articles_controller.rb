2d7705f91f551c9be6295908a699837dbb3ee9b6
Seif <seif.dorani@gmail.com>
2019-04-26 23:03:29 -0700

    styled up flashed up all error handling messages

--- 02-Rails/02-Second_Rails_App/AlphaBlog/app/controllers/articles_controller.rb
+++ 02-Rails/02-Second_Rails_App/AlphaBlog/app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "Article was created"
+      flash[:success] = "Article was created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = "Articles was destroyed!"
+    flash[:danger] = "Articles was destroyed!"
     redirect_to articles_path
   end
 
