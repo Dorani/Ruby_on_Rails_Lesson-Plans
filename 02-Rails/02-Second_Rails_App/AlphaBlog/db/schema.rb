34095598e857a11e1a92f7a72e03b880ccbd7041
Seif <seif.dorani@gmail.com>
2019-05-05 13:29:42 -0700

    create users table and model, migration, and crud in console to test

--- 02-Rails/02-Second_Rails_App/AlphaBlog/db/schema.rb
+++ 02-Rails/02-Second_Rails_App/AlphaBlog/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_171606) do
+ActiveRecord::Schema.define(version: 2019_05_05_202210) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "create_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
