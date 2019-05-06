210d98812f8a509e7eeb153711d70dcd150ba5a5
Seif <seif.dorani@gmail.com>
2019-05-05 21:47:55 -0700

    created new migration for data associations

--- 02-Rails/02-Second_Rails_App/AlphaBlog/db/schema.rb
+++ 02-Rails/02-Second_Rails_App/AlphaBlog/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_202210) do
+ActiveRecord::Schema.define(version: 2019_05_06_044451) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "create_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
