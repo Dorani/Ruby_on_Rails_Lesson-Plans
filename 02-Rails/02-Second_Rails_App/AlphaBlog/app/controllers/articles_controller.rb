ec78c55261f09a072fe1feb1fcfa4badebdd045e
Seif <seif.dorani@gmail.com>
2019-05-05 22:03:21 -0700

    created user article association, in the db

--- 02-Rails/02-Second_Rails_App/AlphaBlog/app/controllers/articles_controller.rb
+++ 02-Rails/02-Second_Rails_App/AlphaBlog/app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was created"
       redirect_to article_path(@article)
