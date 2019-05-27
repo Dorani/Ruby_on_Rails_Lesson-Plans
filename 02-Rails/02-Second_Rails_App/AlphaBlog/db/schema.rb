26202d34dc3ca0477f06685c9db2bfc7f62361a4
Seif <seif.dorani@gmail.com>
2019-05-26 20:26:40 -0700

    db reset, migration with updates, and got the timestamps working

--- 02-Rails/02-Second_Rails_App/AlphaBlog/db/schema.rb
+++ 02-Rails/02-Second_Rails_App/AlphaBlog/db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_05_06_044451) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "create_at"
+    t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
   end
