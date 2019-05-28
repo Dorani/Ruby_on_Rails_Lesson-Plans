ace04f32b1c8f0c5234c2ceaa96a534a88aae296
Seif <seif.dorani@gmail.com>
2019-05-27 23:43:09 -0700

    add edit update functionality and partial for users

--- 02-Rails/02-Second_Rails_App/AlphaBlog/app/controllers/users_controller.rb
+++ 02-Rails/02-Second_Rails_App/AlphaBlog/app/controllers/users_controller.rb
@@ -13,8 +13,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
 
-
+  def update
+    @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = "Your account was updated!"
+        redirect_to articles_path
+      else
+        render 'edit'
+      end
+  end
 
   private
   def user_params
--- 02-Rails/02-Second_Rails_App/AlphaBlog/config/routes.rb
+++ 02-Rails/02-Second_Rails_App/AlphaBlog/config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
-  resource :users, except: [:new]
+  resources :users, except: [:new]
 
 end
